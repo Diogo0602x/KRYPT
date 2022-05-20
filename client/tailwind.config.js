149bd3dd1144b846f94692de842f98cc7d761507
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-05-20 11:42:52 -0300

    welcome page done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,38 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
+  mode: "jit",
   theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
