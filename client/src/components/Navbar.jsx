afe655140e6473f73189c177cab30bd082ba0972
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-05-20 10:34:40 -0300

    Loader done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
-            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt='logo' className="w-32 cursor-pointer"/>
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
@@ -27,12 +27,12 @@ const Navbar = () => {
                 </li>
             </ul>
             <div className='flex relative'>
-                {!toggleMenu 
+                {toggleMenu 
                     ? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)}/>
                     : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)}/>}
                 {toggleMenu && (
                     <ul
-                         className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
+                         className='z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
                     >
                         <li className='text-xl w-full my-2'>
                             <AiOutlineClose onClick={() => setToggleMenu(false)}/>
