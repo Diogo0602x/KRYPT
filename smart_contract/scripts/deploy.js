da4022dffcd50fc294e873465d426bbe9e0bdc52
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-05-20 14:42:35 -0300

    doing deploy

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,20 @@
+const main = async () =>  {
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log("Transaction deployed to:", transactions.address);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+runMain();
