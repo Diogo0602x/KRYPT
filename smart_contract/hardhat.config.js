0a019149a2295f43a1fb95a3fde9632805c76e92
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-05-23 15:44:13 -0300

    transaction deployed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/rVKDTWREAy1EnqaF_dPxnCb_nU88d7u0',
+      accounts: ['b327d2c3a5ada7c0a611821b83daf1cf5ad1855dbb4953c4e009fd699f753484']
+    }
+  }
+}
